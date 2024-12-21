855aee7cb0aea47c3e20e10f9ac1ee2623e3a3fd
hariom.sharma <hariom.sharma@flipkart.com>
2024-12-21 11:58:15 +0530

    [any message]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -11,10 +11,15 @@ func main() {
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 		s, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		s=strings.Split(s,"\n")[0]
 		if s == "exit 0\n" {
 			os.Exit(0)
+		}else if  strings.Contains(s,"echo "){
+				fmt.Println(strings.Split(s,"echo ")[1]);
+		}else{
+			fmt.Fprintf(os.Stdout, "%s: not found\n", s)
 		}
-		fmt.Fprintf(os.Stdout, "%s: not found\n", strings.Split(s, "\n")[0])
+		 
 	}
 
 }
